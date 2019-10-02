965df36e715c6bde7a654382742267034a1f0ff1
sean-kelly <skelstar@gmail.com>
2019-10-03 07:09:47 +1300

    Display working but only half
    commented out line
    https://github.com/Xinyuan-LilyGO/TTGO-T-Display/issues/4

--- User_Setup_Select.h
+++ User_Setup_Select.h
@@ -19,10 +19,9 @@
 
 // Only ONE line below should be uncommented.  Add extra lines and files as needed.
 
-#include <User_Setup.h>           // Default setup is root library folder
+// #include <User_Setup.h>           // Default setup is root library folder
 
 #include <User_Setups/TTGO_T_Display.h>
-
 //#include <User_Setups/Setup1_ILI9341.h>  // Setup file configured for my ILI9341
 //#include <User_Setups/Setup2_ST7735.h>   // Setup file configured for my ST7735
 //#include <User_Setups/Setup3_ILI9163.h>  // Setup file configured for my ILI9163
